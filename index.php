e3fd5ddb06e8600cffaac89086e2a41dabb9c91b
ryooz16 <riopermanam45@gmail.com>
2022-12-21 11:14:36 +0700

    Menamabahkan Paragraf pada file index.php

--- index.php
+++ index.php
@@ -8,5 +8,6 @@
 </head>
 <body>
     <h1>Helllow world</h1>
+    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Adipisci numquam deleniti voluptates quia, eligendi iste, saepe distinctio magni dolorem natus eveniet nisi aliquid placeat quaerat aliquam atque illum itaque quasi? Quia culpa error ad molestias enim nobis, nostrum pariatur eos, exercitationem minus, architecto facilis provident rerum tempore? Recusandae quaerat inventore nostrum aut praesentium voluptatem harum illum incidunt expedita. Aspernatur saepe quo earum deserunt delectus nulla optio ipsum magni numquam dolore? Quasi sint excepturi numquam eveniet temporibus. Soluta iusto fugiat natus temporibus perspiciatis vel, totam facere, sequi impedit velit quis doloribus dolores unde molestias autem consequatur repudiandae repellat quia ducimus quam.</p>
 </body>
 </html>
